16d6a94055acea6ed1dbeb290b4d52c53c768a70
Noel Alexander <noel.alexander@gmail.com>
2013-02-14 15:43:55 -0500

    added compareChoices and finishRound methods

--- Rock Paper Scissors.java
+++ Rock Paper Scissors.java
@@ -6,8 +6,8 @@ package rockpaperscissors;
  * @author noel
  */
 
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
         
 public class RockPaperScissors 
@@ -19,23 +19,29 @@ public class RockPaperScissors
     
 
     
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(System.in);
-        int[] gameTally = new int[3];
+        int[] gameTally = {0, 0, 0};//initialize win counts to all 0s
         String quitGame;
         boolean playGame = true;
-        int userChoice, computerChoice;
+        int userChoice, computerChoice, winner;
          
         while(playGame)
         {
             //play game!
-            
             //user plays
             userChoice = userPlay();
                     
             //computer plays
             computerChoice = computerPlay();
+            
+            //compare player choices
+            winner = compareChoices(userChoice, computerChoice);
+            
+            //complete this round
+            finishRound(winner, gameTally);
             
             System.out.println("Enter Q to quit\nAny other input to keep playing: ");
             quitGame = input.next();
@@ -44,7 +50,7 @@ public class RockPaperScissors
             {playGame=false;}
         }
         
-         System.out.println("end");
+         System.out.println("Goodbye!");
         
     }//end of main method
     
@@ -80,6 +86,57 @@ public class RockPaperScissors
         
         return computerPlayChoice;
     }//end of computerPlay method
-          
+    
+    public static int compareChoices(int usersChoice, int computersChoice)
+    {
+        int winnerInteger = 0;//will return 1 for human, 2 for pc, 3 for tie
+        
+        //player win
+        if( (usersChoice == 1 && computersChoice == 3) ||
+            (usersChoice == 3 && computersChoice == 2) ||
+            (usersChoice == 2 && computersChoice == 1) )
+        {
+            winnerInteger = 1;
+        }
+        
+        //computer win
+        if( (computersChoice == 1 && usersChoice == 3) ||
+            (computersChoice == 3 && usersChoice == 2) ||
+            (computersChoice == 2 && usersChoice == 1) )
+        {
+            winnerInteger = 2;
+        }        
+
+        //tie
+        if(usersChoice == computersChoice)
+        {
+            winnerInteger = 3;            
+        }        
+        
+        return winnerInteger;
+    }//end compareChoices
+    
+    public static void finishRound(int winTemp, int[] gameTally)
+    {//winTemp 1=player, 2=pc, 3=tie
+
+        if(winTemp == 1)
+        {
+            System.out.println("The human won.");
+            gameTally[0]++;
+        }
+        
+        if(winTemp == 2)
+        {
+            System.out.println("The computer won.");
+            gameTally[1]++;            
+        }
+        
+        if(winTemp == 3)
+        {
+            System.out.println("The game was a tie.");
+            gameTally[2]++;
+        }
+        
+    }//end of finishRound
     
 }
